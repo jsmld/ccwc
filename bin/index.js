701e9570afcbf57fa9d73590487b5a5667ba2d5d
jsmld <samvanerpersroijaards@gmail.com>
2024-02-24 23:47:02 -0300

    Adds word flag

--- bin/index.js
+++ bin/index.js
@@ -5,17 +5,22 @@ import fs from 'fs'
 try {
   const args = arg({
     "-c": String,
-    "-l": String
+    "-l": String,
+    "-w": String
   })
 
   const bytesFileName = args['-c']
   const linesFileName = args['-l']
+  const wordsFileName = args['-w']
 
   if (bytesFileName) {
     console.log(fs.statSync(bytesFileName).size, bytesFileName)
   }
   if (linesFileName) {
     console.log(fs.readFileSync(linesFileName).toString().split('\n').length - 1, linesFileName)
+  }
+  if (wordsFileName) {
+    console.log(fs.readFileSync(wordsFileName).toString().trim().split(/\s+/).length, wordsFileName)
   }
 } catch (error) {
   console.error(error.message)
