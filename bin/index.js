09019274d895ee2954e732da18da2504eed44c2c
jsmld <samvanerpersroijaards@gmail.com>
2024-02-26 15:31:23 -0300

    Refactor for potench solution to reading from standard input

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,22 @@
 import arg from 'arg'
 import fs from 'fs'
 
+function byteCount (fileName) {
+  return fs.statSync(fileName).size
+}
+
+function lineCount (fileName) {
+  return fs.readFileSync(fileName).toString().split('\n').length - 1
+}
+
+function wordCount (fileName) {
+  return fs.readFileSync(fileName).toString().trim().split(/\s+/).length
+}
+
+function charCount (fileName) {
+  return fs.readFileSync(fileName).toString().split('').length
+}
+
 try {
   const args = arg({
     "-c": String,
@@ -14,21 +30,22 @@ try {
   const linesFileName = args['-l']
   const wordsFileName = args['-w']
   const charsFileName = args['-m']
+  const defaultFileName = process.argv[2]
 
   if (bytesFileName) {
-    console.log(fs.statSync(bytesFileName).size, bytesFileName)
+    console.log(byteCount(bytesFileName) + ` ${bytesFileName}`)
   }
   if (linesFileName) {
-    console.log(fs.readFileSync(linesFileName).toString().split('\n').length - 1, linesFileName)
+    console.log(lineCount(linesFileName) + ` ${linesFileName}`)
   }
   if (wordsFileName) {
-    console.log(fs.readFileSync(wordsFileName).toString().trim().split(/\s+/).length, wordsFileName)
+    console.log(wordCount(wordsFileName) + ` ${wordsFileName}`)
   }
   if (charsFileName) {
-    console.log(fs.readFileSync(charsFileName).toString().split('').length, charsFileName)
+    console.log(charCount(charsFileName) + ` ${charsFileName}`)
   }
   if (process.argv.length == 3) {
-    console.log(fs.statSync(process.argv[2]).size, fs.readFileSync(process.argv[2]).toString().split('\n').length - 1, fs.readFileSync(process.argv[2]).toString().trim().split(/\s+/).length, process.argv[2])
+    console.log(byteCount(defaultFileName), lineCount(defaultFileName), wordCount(defaultFileName), defaultFileName)
   }
 } catch (error) {
   console.error(error.message)
